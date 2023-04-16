510a805bf1dce065650862459348751aa1a0b164
AlaaKhamisy <130701021+AlaaKhamisy@users.noreply.github.com>
2023-04-16 19:33:45 +0200

    Add files via upload

---
+++ task 5.py
@@ -0,0 +1,69 @@
+
+class Node:
+    def __init__(self, val):
+        self.l = None
+        self.r = None
+        self.v = val
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def getRoot(self):
+        return self.root
+
+    def add(self, val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            self._add(val, self.root)
+
+    def _add(self, val, node):
+        if val < node.v:
+            if node.l is not None:
+                self._add(val, node.l)
+            else:
+                node.l = Node(val)
+        else:
+            if node.r is not None:
+                self._add(val, node.r)
+            else:
+                node.r = Node(val)
+
+    def deleteTree(self):
+        self.root = None
+
+    def printTree(self):
+        if self.root is not None:
+            self._printTree(self.root)
+
+    def _printTree(self, node):
+        if node is not None:
+            self._printTree(node.l)
+            print(str(node.v) + ' ')
+            self._printTree(node.r)            
+
+   
+#     3
+# 0     4
+#   2      8
+tree = Tree()
+tree.add(3)
+tree.add(4)
+tree.add(0)
+tree.add(8)
+tree.add(2)
+tree.printTree()
+
+
+choice = int(input("Enter your choice no.:"))
+if choice == 1:
+    insertion =int(input("Enter the number to add:"))  
+    tree.add(insertion)
+    tree.printTree()
+    
+elif choice == 2:
+    deletion =  int(input("Enter the number to remove:"))
+    tree.deleteTree(deletion) 
+    tree.printTree()
+
